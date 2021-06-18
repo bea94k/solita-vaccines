b088ef43deba3a291af7462ce0a130a76d49dc94
bea94k <beatakusnierz94@gmail.com>
2021-06-18 14:12:57 +0300

    MySQL connection setup

---
+++ backend/functions/mysql/connection.js
@@ -0,0 +1,44 @@
+const mysql = require("mysql");
+
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "solitaVaccines",
+});
+
+exports.startConnection = async () => {
+  connection.connect(function (err) {
+    if (err) {
+      console.error("error: " + err.message);
+      return {
+        statusCode: 500,
+        body: "Something went wrong when connecting to MySQL server. Check backend console for error message.",
+      };
+    }
+
+    console.log("Connected to the MySQL server.");
+    return {
+      statusCode: 200,
+      body: "Connected to MySQL server.",
+    };
+  });
+};
+
+exports.endConnection = async () => {
+  connection.end(function (err) {
+    if (err) {
+      console.error("error: " + err.message);
+      return {
+        statusCode: 500,
+        body: "Something went wrong when ending the connection to MySQL server. Check backend console for error message.",
+      };
+    }
+
+    console.log("Closing the database connection.");
+    return {
+      statusCode: 200,
+      body: "Closing the connection to MySQL server.",
+    };
+  });
+};
