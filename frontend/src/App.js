e944cc2a5e7264b17e343d3dcd7fda02126c34c8
bea94k <beatakusnierz94@gmail.com>
2021-06-20 12:13:39 +0300

    Front: fetch all time data when the app loads

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import axios from "./axios";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const [allTimeData, setAllTimeData] = useState();
+
+  const fetchAllData = () => {
+    console.log("button clicked");
+    axios
+      .get("/all-time-data")
+      .then((resp) => {
+        console.log("general data", resp.data.allTimeData);
+        setAllTimeData(resp.data.allTimeData);
+      })
+      .catch((err) => {
+        console.log("error", err);
+      });
+  };
+
+  useEffect(() => {
+    fetchAllData();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Vaccination stats</h1>
+      {!allTimeData ? <p>No data yet.</p> : <p>Data received.</p>}
     </div>
   );
-}
+};
 
 export default App;
